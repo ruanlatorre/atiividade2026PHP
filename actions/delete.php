604d1d35feca0ea15071acc4631e804e4adc9186
ruanlatorre <ruan.latorre@aluno.senai.br>
2026-01-29 17:01:39 -0300

    mudanças no codigo de deletar

--- actions/delete.php
+++ actions/delete.php
@@ -5,7 +5,16 @@ if (isset($_GET['id'])) {
     $id = $_GET['id'];
 }
 
-$sql = "DELETE FROM pessoa WHERE id_pessoa = '$id'";
-$result = mysqli_query($conexao, $sql);
+$sql = "DELETE FROM pessoa WHERE id_pessoa = ?";
+$stmt = mysqli_prepare($conexao, $sql);
+mysqli_stmt_bind_param($stmt, 'i', $id);
+
+if (mysqli_stmt_execute($stmt)) {
+    header("Location: ../actions/consult.php");
+    exit;
+} else {
+    echo "Erro ao deletar a pessoa: " . mysqli_stmt_error($stmt);
+}
+mysqli_stmt_close($stmt);
 
 ?>
